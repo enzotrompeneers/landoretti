7019cae2aff4b0362f2ccb57fb3d444b4224aa7c
enzotrompeneers <trompeneers@telenet.be>
2017-12-01 19:45:50 +0100

    header: top line, logo & logo container, personal options and installed font awesome

--- landoretti/resources/views/lander/index.blade.php
+++ landoretti/resources/views/lander/index.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>Landoretti</h1>
+	<div class="line-top"></div>
+	<div class="header">
+		<div class="header-logo-container">
+			<div class="header-logo">
+				<a href="{{ route('lander.index') }}"><img src="{{ asset('assets/graphics/landoretti-art.png') }}" alt="landoretti art"></a>
+			</div>
+			
+		</div>
+		
+		<div class="header-personal-options">
+			<ul>
+				<li><i class="fa fa-bars" aria-hidden="true"></i><a href="">Watchlist</a></li>
+				<li><a href="">Profile</a></li>
+				<li><a href="">Logout</a></li>
+			</ul>
+		</div>
+	</div>
 @stop
