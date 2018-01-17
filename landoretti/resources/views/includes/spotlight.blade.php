402b8005b081aa8d9b7e8340737b406c45e70890
enzotrompeneers <trompeneers@telenet.be>
2018-01-17 18:38:08 +0100

    spotlight

--- landoretti/resources/views/includes/spotlight.blade.php
+++ landoretti/resources/views/includes/spotlight.blade.php
@@ -1,1 +1,14 @@
-<h1>spotlight</h1>
+<section class="spotlight">
+    <div class="container">
+        <div class="container-idea">
+            <div class="header">
+                <div class="art-icon"></div>
+                <h1>Lorem Ipsum</h1>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. magnis dis parturient montes, nascetur ridiculus mus...</p>
+                <span class="price">Price:</span> 
+                <span class="amount">&euro; 299,99</span>
+            </div>
+            <a href="#" class="btn-visit-auction">visit auction<span></span></a>
+        </div>
+    </div>
+</section>
--- landoretti/resources/views/search/index.blade.php
+++ landoretti/resources/views/search/index.blade.php
@@ -1,11 +1,21 @@
 @extends('layouts.master')
 
 @section('content')
+	@include('includes.navigation')
 	@include('includes.spotlight')
 @stop
 
 @section('scripts')
-
+<script>
+	$(document).ready(function(){
+		// Login States
+		$('#login').click(function(){
+			$('.state-interaction').show();
+			$('.state-not-logged-in').hide();
+		});
+		// End Login States
+	});
+</script>
 @stop
 
 
